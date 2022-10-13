01b895ef4b43e44d9a6700bbdd2c8640f80b0cb3
Edita Marija <52567746+EditaMarija@users.noreply.github.com>
2022-10-13 14:32:18 +0200

    Initial commit

---
+++ TodoApp/Model/Todo.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoApp.Model
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+
+        public DateTime? DoneDate { get; set; }
+
+        public Todo()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Todo(string text) : this()
+        {
+            Text = text;
+        }
+    }
+}
---
+++ TodoApp/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using TodoApp;
+using TodoApp.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+//var todos = new List<Todo>()
+//{
+//    new Todo("Male huset"),
+//    new Todo("Skifte til vinterdekk"),
+//    new Todo("Snekre terrasse")
+//};
+
+var json = File.ReadAllText("todos.json");
+var todos = JsonSerializer.Deserialize<List<Todo>>(json);
+
+app.MapGet("/todo/{id}", (Guid id) =>
+{
+    return todos.SingleOrDefault(t => t.Id == id);
+});
+
+app.MapGet("/todo", () =>
+{
+    return todos;
+});
+
+app.MapPut("/todo", async (Todo todo) =>
+{
+    var todoInList = todos.SingleOrDefault(t => t.Id == todo.Id);   
+    todoInList.DoneDate = DateTime.Today;
+    var json = JsonSerializer.Serialize(todos);
+    await File.WriteAllTextAsync("todos.json", json);
+});
+
+app.MapPost("/todo", async (Todo todo) =>
+{
+    todos.Add(todo);
+    var json = JsonSerializer.Serialize(todos);
+    await File.WriteAllTextAsync("todos.json", json);
+});
+
+app.MapDelete("/todo/{id}", async (string id) =>
+{
+    var guidId = Guid.Parse(id);
+    var todoInList = todos.FirstOrDefault(t => t.Id == guidId);
+    if (todoInList == null) throw new NullReferenceException();
+    todos.Remove(todoInList);
+    var json = JsonSerializer.Serialize(todos);
+    //for (int i = 0; i < todos.Count; i++)
+    //{
+    //    if(todos[i].Id == guidId)
+    //    {
+    //        todos.Remove(todos[i]);
+    //    }
+    //}
+    await File.WriteAllTextAsync("todos.json", json);
+});
+
+app.Run();
